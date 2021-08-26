b02a7f4ec1a4b41d48c1bfd5c661b85ad4156c67
NatalieTanML <nat130613@gmail.com>
2021-08-26 13:19:49 +0800

    Lab1: peasoup

--- src/Lab1/peasoup.java
+++ src/Lab1/peasoup.java
@@ -1,4 +1,40 @@
 package Lab1;
 
+/**
+ * Tan Ming Li, Natalie
+ * A0220822U
+ */
+
+import java.util.*;
+
 public class peasoup {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine()); // no. of restaurants
+        boolean found = false;
+        String name = "";
+
+        while (n-- > 0 && !found) {
+            int k = Integer.parseInt(sc.nextLine()); // no. of menu items
+            String input = sc.nextLine(); // name of restaurant;
+            boolean ps = false, pc = false;
+            while (k-- > 0) {
+                String item = sc.nextLine();
+                if (item.equals("pea soup"))
+                    ps = true;
+                else if (item.equals("pancakes"))
+                    pc = true;
+            }
+            if (ps && pc) {
+                found = true;
+                name = input;
+            }
+        }
+
+        if (found)
+            System.out.println(name);
+        else
+            System.out.println("Anywhere is fine I guess");
+
+    }
 }
